9aa27f0145b587d3c30cc44384c6103b5430cfcd
madsemilgodstil <mads93n2@stud.kea.dk>
2024-12-04 14:32:43 +0100

    related

--- src/app/pages/artist/page.js
+++ src/app/pages/artist/page.js
@@ -29,6 +29,14 @@ import {
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
+function getRelatedBands (bands, currentBand) {
+  const related = bands.filter(
+    band => band.genre === currentBand.genre && band.name !== currentBand.name
+  )
+
+  return related.slice(0, 3)
+}
+
 async function displayArtists () {
   const bands = await getBands()
 
@@ -39,55 +47,111 @@ async function displayArtists () {
           All Artists
         </h1>
         <div className='grid grid-cols-3 gap-8'>
-          {bands.map(band => (
-            <Card>
-              <CardHeader>
-                <CardTitle className='text-center text-xl'>
-                  {band.name}
-                </CardTitle>
-                <CardDescription className='text-center'>
-                  {band.genre}
-                </CardDescription>
-              </CardHeader>
-              <CardContent className='flex justify-center'>
-                {/* Avatar */}
-                <Avatar className='w-48 h-48 rounded-full'>
-                  <AvatarImage src={band.logo} alt={band.name} />
-                  <AvatarFallback>{band.name}</AvatarFallback>
-                </Avatar>
-              </CardContent>
-              <CardContent></CardContent>
-              <CardFooter className='flex justify-center'>
-                {/* <p className='text-xs px-10 py-5'>{band.bio}</p> */}
-                <Sheet>
-                  <SheetTrigger>View Artist</SheetTrigger>
-                  <SheetContent>
-                    <SheetHeader>
-                      <SheetTitle className='text-center'>
-                        {band.name}
-                      </SheetTitle>
-                      <SheetDescription className='text-center'>
-                        {band.genre}
-                      </SheetDescription>
-                      <SheetDescription className='text-center'>
-                        {Array.isArray(band.members)
-                          ? band.members.join(', ')
-                          : band.members}
-                      </SheetDescription>
-                      <SheetDescription className='flex justify-center'>
-                        {' '}
-                        <Avatar className='w-48 h-48 rounded-full'>
-                          <AvatarImage src={band.logo} alt={band.name} />
-                          <AvatarFallback>{band.name}</AvatarFallback>
-                        </Avatar>
-                      </SheetDescription>
-                      <SheetDescription>{band.bio}</SheetDescription>
-                    </SheetHeader>
-                  </SheetContent>
-                </Sheet>
-              </CardFooter>
-            </Card>
-          ))}
+          {bands.map(band => {
+            const relatedBands = getRelatedBands(bands, band)
+
+            return (
+              <Card key={band.id}>
+                <CardHeader>
+                  <CardTitle className='text-center text-xl'>
+                    {band.name}
+                  </CardTitle>
+                  <CardDescription className='text-center'>
+                    {band.genre}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className='flex justify-center'>
+                  {/* Avatar */}
+                  <Avatar className='w-48 h-48 rounded-full'>
+                    <AvatarImage src={band.logo} alt={band.name} />
+                    <AvatarFallback>{band.name}</AvatarFallback>
+                  </Avatar>
+                </CardContent>
+                <CardFooter className='flex justify-center'>
+                  <Sheet>
+                    <SheetTrigger>View Artist</SheetTrigger>
+                    <SheetContent>
+                      <SheetHeader>
+                        <SheetTitle className='text-center'>
+                          {band.name}
+                        </SheetTitle>
+                        <SheetDescription className='text-center'>
+                          {band.genre}
+                        </SheetDescription>
+                        <SheetDescription className='text-center'>
+                          {Array.isArray(band.members)
+                            ? band.members.join(', ')
+                            : band.members}
+                        </SheetDescription>
+                        <SheetDescription className='flex justify-center'>
+                          <Avatar className='w-48 h-48 rounded-full'>
+                            <AvatarImage src={band.logo} alt={band.name} />
+                            <AvatarFallback>{band.name}</AvatarFallback>
+                          </Avatar>
+                        </SheetDescription>
+                        <SheetDescription>{band.bio}</SheetDescription>
+                        <SheetDescription>
+                          <h2 className='py-8 text-center'>Related</h2>
+                          <ul className='text-center space-y-2'>
+                            {relatedBands.map(related => (
+                              <li key={related.name}>
+                                <Sheet>
+                                  <SheetTrigger>{related.name}</SheetTrigger>
+                                  <SheetContent>
+                                    <SheetHeader>
+                                      <SheetTitle className='text-center'>
+                                        {band.name}
+                                      </SheetTitle>
+                                      <SheetDescription className='text-center'>
+                                        {band.genre}
+                                      </SheetDescription>
+                                      <SheetDescription className='text-center'>
+                                        {Array.isArray(band.members)
+                                          ? band.members.join(', ')
+                                          : band.members}
+                                      </SheetDescription>
+                                      <SheetDescription className='flex justify-center'>
+                                        <Avatar className='w-48 h-48 rounded-full'>
+                                          <AvatarImage
+                                            src={band.logo}
+                                            alt={band.name}
+                                          />
+                                          <AvatarFallback>
+                                            {band.name}
+                                          </AvatarFallback>
+                                        </Avatar>
+                                      </SheetDescription>
+                                      <SheetDescription>
+                                        {band.bio}
+                                      </SheetDescription>
+                                      <SheetDescription>
+                                        <h2 className='py-8 text-center'>
+                                          Related
+                                        </h2>
+                                        <ul className='text-center space-y-2'>
+                                          {relatedBands.map(related => (
+                                            <li key={related.name}>
+                                              <SheetTrigger>
+                                                {related.name}
+                                              </SheetTrigger>
+                                            </li>
+                                          ))}
+                                        </ul>
+                                      </SheetDescription>
+                                    </SheetHeader>
+                                  </SheetContent>
+                                </Sheet>
+                              </li>
+                            ))}
+                          </ul>
+                        </SheetDescription>
+                      </SheetHeader>
+                    </SheetContent>
+                  </Sheet>
+                </CardFooter>
+              </Card>
+            )
+          })}
         </div>
       </div>
       <div className='fixed bottom-5 right-5'>
