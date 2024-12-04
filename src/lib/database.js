a82838de1f452ae9b4814322de0f6441ee7b7507
madsemilgodstil <mads93n2@stud.kea.dk>
2024-12-04 11:21:12 +0100

    db edit

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,74 +1,74 @@
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL
 
 const headersList = {
-  Accept: "*/*",
-  "Content-Type": "application/json",
-  "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+  Accept: '*/*',
+  'Content-Type': 'application/json',
+  'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
   apikey: key,
-  Prefer: "return=representation",
-};
+  Prefer: 'return=representation'
+}
 
 //Get
-export async function getSubs() {
+export async function getSubs () {
   const response = await fetch(url, {
-    method: "GET",
-    headers: headersList,
-  });
+    method: 'GET',
+    headers: headersList
+  })
 
-  const data = await response.json();
-  return data;
+  const data = await response.json()
+  return data
 }
 
-//GET enkel
-export async function getSubById(id) {
-  const response = await fetch(`${url}?id=eq.${id}`, {
-    method: "GET",
-    headers: headersList,
-  });
+// //GET enkel
+// export async function getSubById(id) {
+//   const response = await fetch(`${url}?id=eq.${id}`, {
+//     method: "GET",
+//     headers: headersList,
+//   });
 
-  const data = await response.json();
-  return data;
-}
+//   const data = await response.json();
+//   return data;
+// }
 
-//Post
-export async function postSub(subscriber) {
-  const response = await fetch(url, {
-    method: "POST",
-    headers: headersList,
-    body: JSON.stringify(subscriber),
-  });
+// //Post
+// export async function postSub(subscriber) {
+//   const response = await fetch(url, {
+//     method: "POST",
+//     headers: headersList,
+//     body: JSON.stringify(subscriber),
+//   });
 
-  const data = await response.json();
-  return data;
-}
+//   const data = await response.json();
+//   return data;
+// }
 
-//Patch
-export async function patchSub(id, dataToUpdate) {
-  const response = await fetch(`${url}?id=eq.${id}`, {
-    method: "PATCH",
-    headers: headersList,
-    body: JSON.stringify(dataToUpdate),
-  });
+// //Patch
+// export async function patchSub(id, dataToUpdate) {
+//   const response = await fetch(`${url}?id=eq.${id}`, {
+//     method: "PATCH",
+//     headers: headersList,
+//     body: JSON.stringify(dataToUpdate),
+//   });
 
-  const data = await response.json();
-  return data;
-}
+//   const data = await response.json();
+//   return data;
+// }
 
-//Delete
-export async function deleteSub(id) {
-  const response = await fetch(`${url}?id=eq.${id}`, {
-    method: "DELETE",
-    headers: {
-      apikey: key,
-      Prefer: "return=representation",
-    },
-  });
-  // eksempel på håndtering af svar
-  if (response.ok) {
-    console.log("Resource deleted successfully");
-  } else {
-    console.error("Failed to delete resource");
-  }
-  const data = await response.json();
-  return data;
-}
+// //Delete
+// export async function deleteSub(id) {
+//   const response = await fetch(`${url}?id=eq.${id}`, {
+//     method: "DELETE",
+//     headers: {
+//       apikey: key,
+//       Prefer: "return=representation",
+//     },
+//   });
+//   // eksempel på håndtering af svar
+//   if (response.ok) {
+//     console.log("Resource deleted successfully");
+//   } else {
+//     console.error("Failed to delete resource");
+//   }
+//   const data = await response.json();
+//   return data;
+// }
