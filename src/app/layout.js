8a984a980e5d0552aa46f22fa10f4c1bf677fd6e
madsemilgodstil <mads93n2@stud.kea.dk>
2024-12-05 13:22:55 +0100

    edit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,7 @@ import localFont from 'next/font/local'
 import './globals.css'
 import Navigation from '@/components/navigation/Navigation'
 import Footer from '@/components/footer/Footer'
-import Link from 'next/link'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger
-} from '@/components/ui/tooltip'
+import ScrollToTop from '@/components/scrolltotop/ScrollToTop' // Import the new component
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -35,18 +29,7 @@ export default function RootLayout ({ children }) {
         <Navigation />
         {children}
         <Footer />
-        <div className='fixed bottom-5 right-5'>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger>
-                <Link href='/pages/artist'>Top</Link>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Go to top</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </div>
+        <ScrollToTop />
       </body>
     </html>
   )
---
+++ src/components/scrolltotop/ScrollToTop.jsx
@@ -0,0 +1,37 @@
+"use client"; // Mark this as a client component
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export default function ScrollToTop() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Smooth scrolling animation
+    });
+  };
+
+  return (
+    <div className="fixed bottom-5 right-5">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <button
+              onClick={scrollToTop}
+              className="bg-blue-500 text-white px-4 py-2 rounded shadow-md"
+            >
+              Top
+            </button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Go to top</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+}
