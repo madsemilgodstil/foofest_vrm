30ddc17478d5a5729103e80e9cf9d3ec64c2f3d0
madsemilgodstil <mads93n2@stud.kea.dk>
2024-12-12 11:58:53 +0100

    ddd

--- src/components/hero_landing/Hero_landing.jsx
+++ src/components/hero_landing/Hero_landing.jsx
@@ -6,41 +6,45 @@ function HeroLanding() {
   return (
     <div className="relative h-screen">
       <h1 className="hidden">Hero Landing</h1>
-      {/* Styled h2 */}
-      <h2 className="absolute inset-x-0 top-[15%] text-center text-5xl sm:text-7xl lg:text-9xl font-black text-white z-10">
-        FooFest 2025
-      </h2>
+
       {/* Image Container */}
-      <div className="relative h-screen w-full">
+      <div className="relative h-full w-full">
         <Image
           src="https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Festival Image"
           layout="fill"
           objectFit="cover"
           priority
+          className="z-0"
         />
       </div>
-      {/* Button Container */}
-      <div className="absolute inset-0 flex flex-col items-center justify-center pb-[10%]">
-        <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
-          {/* Line-up Button */}
-          <Link href="/pages/artist">
-            <Button
-              variant="default" // Brug variant "default" til at få bg-primary og text-white
-              className="px-10 py-2 rounded-full hover:opacity-85"
-            >
-              Line-up
-            </Button>
-          </Link>
-          {/* Tickets Button */}
-          <Link href="/pages/booking">
-            <Button
-              variant="default" // Brug variant "default" til at få bg-primary og text-white
-              className="px-10 py-2 rounded-full hover:opacity-85"
-            >
-              Tickets
-            </Button>
-          </Link>
+
+      {/* Overlay and Content Wrapper */}
+      <div className="absolute inset-0 bg-black/50 flex items-center justify-center z-10">
+        <div className="flex flex-col items-center space-y-8 mb-44">
+          <h2 className="text-5xl sm:text-7xl lg:text-9xl font-extrabold text-white font-moiraione z-20">
+            FooFest 2025
+          </h2>
+          <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 z-20">
+            {/* Line-up Button */}
+            <Link href="/pages/artist">
+              <Button
+                variant="default"
+                className="px-10 py-2 rounded-full hover:opacity-85"
+              >
+                Line-up
+              </Button>
+            </Link>
+            {/* Tickets Button */}
+            <Link href="/pages/booking">
+              <Button
+                variant="default"
+                className="px-10 py-2 rounded-full hover:opacity-85"
+              >
+                Tickets
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
